772e31f1614dd7057ad3de9bbab6f7058b92da94
Hubert Figuière <hub@figuiere.net>
2025-05-20 21:48:51 -0400

    init: Use -e for existing and show relative path
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -11,7 +11,7 @@ use crate::{Error, ErrorContext, Result};
 
 #[derive(Parser)]
 pub struct Args {
-    #[arg(long, help = "Initialize project even if git repository exists")]
+    #[arg(short, long, help = "Initialize project even if git repository exists")]
     /// Initialize if existing.
     existing: bool,
     #[arg(short, long, help = "The application id. Otherwise it is extrapolated")]
@@ -25,7 +25,7 @@ pub struct Args {
 pub fn run(args: Args) -> Result<()> {
     let current_dir = std::env::current_dir()?;
 
-    let target_dir = PathBuf::from(&current_dir).join(args.path);
+    let target_dir = PathBuf::from(&current_dir).join(&args.path);
     if !args.existing && Project::exists(&target_dir) {
         return Err(Error::AlreadyExist(ErrorContext::Project).into());
     }
@@ -51,6 +51,6 @@ pub fn run(args: Args) -> Result<()> {
         r => r,
     }?;
 
-    println!("Created project and git repository at {target_dir:?}");
+    println!("Created project and git repository at {:?}", &args.path);
     Ok(())
 }
